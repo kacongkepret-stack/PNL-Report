6164d96bff63f16f306839aea69a8fd4fc3952ac
Joblagan <sukawidarsa@gmail.com>
2026-05-25 12:55:57 +0800

    Fix favicon route to return Response

--- app/icon.tsx
+++ app/icon.tsx
@@ -1,27 +1,14 @@
-import React from 'react';
+import { NextResponse } from 'next/server';
 
-export default function Icon() {
-  return (
-    <svg
-      width="32"
-      height="32"
-      viewBox="0 0 32 32"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      {/* Background rounded rectangle */}
+export const runtime = 'edge';
+export const contentType = 'image/svg+xml';
+
+export function GET() {
+  const svg = `
+    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
       <rect width="32" height="32" rx="8" fill="url(#bgGradient)" />
-      
-      {/* Stylized "L" letter */}
-      <path
-        d="M22 8H12C10.8954 8 10 8.89543 10 10V24H14V18H20V24H24V10C24 8.89543 23.1046 8 22 8Z"
-        fill="white"
-        fillRule="evenodd"
-      />
-      
-      {/* Accent bar di bawah */}
+      <path d="M22 8H12C10.8954 8 10 8.89543 10 10V24H14V18H20V24H24V10C24 8.89543 23.1046 8 22 8Z" fill="white" fillRule="evenodd" />
       <rect x="10" y="22" width="14" height="2" rx="1" fill="#0ea5e9" opacity="0.3" />
-
       <defs>
         <linearGradient id="bgGradient" x1="0" y1="0" x2="32" y2="32" gradientUnits="userSpaceOnUse">
           <stop stopColor="#0ea5e9" />
@@ -29,5 +16,9 @@ export default function Icon() {
         </linearGradient>
       </defs>
     </svg>
-  );
+  `;
+
+  return new NextResponse(svg, {
+    headers: { 'Content-Type': 'image/svg+xml' },
+  });
 }
